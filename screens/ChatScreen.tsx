feb7ecf790fdca8f769d4fac30aec7af2780a1ba
luciofondon98 <luciofondon98@gmail.com>
2021-07-17 16:47:56 -0400

    Fetch a la API listo, falta mostrar bien msjs en el chat

--- screens/ChatScreen.tsx
+++ screens/ChatScreen.tsx
@@ -22,9 +22,48 @@ export default function ChatScreen() {
       },
     ])
   }, [])
-  
-    const onSend = useCallback((messages = []) => {
-    setMessages(previousMessages => GiftedChat.append(previousMessages, messages))
+
+  const onSend = useCallback((messages = []) => {
+	
+	setMessages(previousMessages => GiftedChat.append(previousMessages, messages)) // añade los mensajes del usuario en el chat
+
+	console.log(messages[0].text); // mensaje enviado por usuario, enviar a la API
+
+	let usr_msj  = messages[0].text
+	const requestOptions = { 
+		method: 'POST',
+		headers: { 'Content-Type': 'application/json' },
+		body: JSON.stringify({
+				msg: usr_msj,
+				uid: 1234
+		})
+	}
+	try { // se envia mensaje a la api
+		return fetch('http://gepartner-app.herokuapp.com/api/', requestOptions)
+		.then(response => {return response.json();})
+		.then(data => {
+			console.log(data)
+			console.log(messages)
+
+		let openai_response = {
+			_id: Math.floor(Math.random() * 10000) + 1,
+			text: data.msg,
+			createdAt: new Date(),
+			user: {
+				_id: 2,
+				name: 'React Native',
+				avatar: 'https://placeimg.com/140/140/any',
+			},
+		} as any
+		
+		setMessages(messages => GiftedChat.append(openai_response, messages))
+		});
+	}
+	
+	catch (error){
+		console.error(error);
+	}
+
   }, [])
 
 
@@ -39,19 +78,19 @@ export default function ChatScreen() {
   )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+//   title: {
+//     fontSize: 20,
+//     fontWeight: 'bold',
+//   },
+//   separator: {
+//     marginVertical: 30,
+//     height: 1,
+//     width: '80%',
+//   },
+// });
