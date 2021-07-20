d78ecb25d4451fb80a2795fac44e67a379b13b9d
Ian Perez <XenZone489@gmail.com>
2021-07-20 05:43:28 -0400

    Cambio apariencia seleccion de lenguaje

--- screens/LanguageScreen.tsx
+++ screens/LanguageScreen.tsx
@@ -1,78 +1,108 @@
 import React, { useCallback, useEffect, useState } from 'react';
 import { FlatList, StyleSheet } from 'react-native';
 
-import { Container, FlagImg, FlagImgWrapper, FlagInfo, FlagText } from '../styles/FlagStyle';
-import { Card, TextSection, UserName } from '../styles/MessageStyle';
-
+import { Card, Container, FlagImg, FlagImgWrapper, FlagInfo, FlagText, Language, TextSection } from '../styles/FlagStyle';
 
 export default function LanguageScreen() {
 
+  // El estado inicial esta en español por default, pero la idea seria poder guardar los 
+  // ajustes del usuario para que se mantenga en memoria persistente
+  const [selected, changeSelected] = useState(0);
+  
   const [Languages, swapLanguages] = useState([
     {
       id: '0',
       Lang: 'Español',
       FlagImg: require('../assets/flags/Spain-Flag-icon.png'),
+      bgColor: 'white',
     },
     {
       id: '1',
       Lang: 'English',
       FlagImg: require('../assets/flags/United-Kingdom-Flag-icon.png'),
+      bgColor: 'white',
     },
     {
       id: '2',
       Lang: 'Français',
       FlagImg: require('../assets/flags/France-Flag-icon.png'),
+      bgColor: 'white',
     },
     {
       id: '3',
       Lang: 'Deutsche',
       FlagImg: require('../assets/flags/Germany-Flag-icon.png'),
+      bgColor: 'white',
     },
     {
       id: '4',
       Lang: 'Suomi',
       FlagImg: require('../assets/flags/Finland-Flag-icon.png'),
+      bgColor: 'white',
     },
     {
       id: '5',
       Lang: 'Pусский',
       FlagImg: require('../assets/flags/Russia-Flag-icon.png'),
+      bgColor: 'white',
     },
     {
       id: '6',
       Lang: 'Italiano',
       FlagImg: require('../assets/flags/Italy-Flag-icon.png'),
+      bgColor: 'white',
     },
   ]);
 
   function setLanguage(LangId: string, lang: string, flag: any) {
 
-    var newLanguages = Languages.filter(item => item.id !== LangId);
-    newLanguages.unshift({ id: LangId, Lang: lang, FlagImg: flag });
+    var index: number = +LangId;
+
+    var newLanguages = Languages;
+    var current = newLanguages[selected];
+    
+    newLanguages[selected] = { id: current.id, Lang: current.Lang, FlagImg: current.FlagImg, bgColor: 'white' }
+    changeSelected(index);
+    
+    newLanguages[index] = { id: LangId, Lang: lang, FlagImg: flag, bgColor: '#00bf4980' }
+    
     swapLanguages(newLanguages);
+
+    //var newLanguages = Languages.filter(item => item.id !== LangId);
+    //newLanguages.sort((a, b) => (+a.id < +b.id ? -1 : 1));
+    //newLanguages.unshift({ id: LangId, Lang: lang, FlagImg: flag });
+    //swapLanguages(newLanguages);
   }
 
 
   return (
     <Container>
+
       <FlatList 
         data={Languages}
         keyExtractor={item=>item.id}
-        renderItem={({item}) => (
-          <Card onPress={() => setLanguage(item.id, item.Lang, item.FlagImg)}>
-            <FlagInfo>
-              <FlagImgWrapper>
-                <FlagImg source={item.FlagImg} />
-              </FlagImgWrapper>
-              <TextSection>
-                <FlagText>
-                  <UserName>{item.Lang}</UserName>
-                </FlagText>
-              </TextSection>
-            </FlagInfo>
+        renderItem={({ item }) => (
+          
+          <Card onPress={() => setLanguage(item.id, item.Lang, item.FlagImg)} style={{backgroundColor: item.bgColor}}>
+          
+          
+              <FlagInfo>
+                
+                <FlagImgWrapper >
+                  <FlagImg source={item.FlagImg} />
+                </FlagImgWrapper>
+          
+                <TextSection>
+                  <FlagText>
+                    <Language>{item.Lang}</Language>
+                  </FlagText>
+                </TextSection>
+
+              </FlagInfo>
           </Card>
         )}
       />
+
     </Container>
   );
 }
@@ -93,5 +123,9 @@ const styles = StyleSheet.create({
     marginVertical: 30,
     height: 1,
     width: '80%',
+  },
+
+  selectedItem: {
+    marginVertical: 80,
   },
 });
--- styles/FlagStyle.js
+++ styles/FlagStyle.js
@@ -3,20 +3,22 @@ import styled from 'styled-components/native';
 export const Container = styled.View`
   flex: 1;
   padding-top: 10px;
-  padding-left: 30px;
-  padding-right: 20px;
-  align-items: center;
+  padding-left: 0px;
+  padding-right: 0px;
   background-color: #ffffff;
 `;
+// align-items: center;
+ 
 
 export const FlagImgWrapper = styled.View`
   padding-top: 15px;
   padding-bottom: 15px;
-`;
+  padding-left: 25px;
+  `;
 
 export const FlagImg = styled.Image`
-  width: 60px;
-  height: 60px;
+  width: 50px;
+  height: 50px;
 `;
 
 export const FlagInfo = styled.View`
@@ -28,4 +30,25 @@ export const FlagText = styled.View`
   flex-direction: row;
   justify-content: space-between;
   margin-bottom: 5px;
+  align-items: center;
+`;
+
+export const Language = styled.Text`
+  font-size: 14px;
+  font-weight: bold;
+`;
+
+export const Card = styled.TouchableOpacity`
+  width: 100%;
+`;
+
+export const TextSection = styled.View`
+  flex-direction: column;
+  justify-content: center;
+  padding: 15px;
+  padding-left: 0;
+  margin-left: 10px;
+  width: 300px;
+  border-bottom-width: 1px;
+  border-bottom-color: #cccccc;
 `;
